51f39e40291e7273d2ce7e2ba49ec79c7104820e
Nicolas Delahaie <nico601.delahaie@gmail.com>
2023-02-23 00:40:15 +0100

    Creation fichier pour gerer le bdd

---
+++ src/scripts/accesBDD.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @brief Retourne le resultat de la requete mise en paremetres
+ * @param $_POST["req"] la requete en question
+ * @return codeRetour, [msgErreur], [reponse] 
+ */
+    
+
+
+
+?>
